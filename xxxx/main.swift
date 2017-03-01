352920f814a5b55fb4201bdf77ae8fbca5676241
mnb3916 <912987977@qq.com>
2017-03-01 11:13:42 +0800

    Initial Commit

---
+++ xxxx/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  xxxx
+//
+//  Created by s20151104708 on 17/3/1.
+//  Copyright © 2017年 s20151104708. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
