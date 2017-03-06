2da7d29c92d6c2cd54510d9ed4ea1ea676b63554
mnb3916 <912987977@qq.com>
2017-03-06 10:57:27 +0800

    1～10从大到小的序

--- xxxx/main.swift
+++ xxxx/main.swift
@@ -8,5 +8,21 @@
 
 import Foundation
 
-print("Hello, World!")
+var a :Array = [6,7,8,4,1,5,3,9,0,2]
+var i = 0
+var j = i + 1
+var n = 0
+for i in 0..<10{
+    for j in 0..<10{
+        if a[i] < a[j] {
+            n = a[i]
+            a[i] = a[j]
+            a[j] = n
+        }
+    }
+}
+      print(a)
 
+
+a.sort()
+print(a)
